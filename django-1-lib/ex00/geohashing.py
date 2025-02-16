51d9c91601303776ec4b85f79bc783e96e41b481
davidmonteiro03 <damonteiro12446@gmail.com>
2025-02-16 15:32:53 +0000

    git updates

---
+++ django-1-lib/ex00/geohashing.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import sys
+import antigravity
+
+
+def main():
+    argc = len(sys.argv)
+    try:
+        assert argc == 4, \
+            f"Usage: python3 {sys.argv[0]} <latitude> <longitude> <datedow>"
+        try:
+            latitude = float(sys.argv[1])
+        except Exception:
+            raise Exception("Error reading latitude.")
+        try:
+            longitude = float(sys.argv[2])
+        except Exception:
+            raise Exception("Error reading longitude.")
+        try:
+            datedow = str(sys.argv[3]).encode()
+        except Exception:
+            raise Exception("Error reading datedow.")
+        try:
+            antigravity.geohash(latitude, longitude, datedow)
+        except Exception:
+            raise Exception("Error doing geohash.")
+    except Exception as e:
+        print(e, file=sys.stderr)
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
