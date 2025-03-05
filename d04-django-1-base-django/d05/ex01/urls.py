a80b6cec29b869d32d099791c59d9514105da390
davidmonteiro03 <damonteiro12446@gmail.com>
2025-03-05 07:50:45 +0000

    git updates

--- d04-django-1-base-django/d05/ex01/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from . import views
-
-urlpatterns = [
-    re_path(r'^django/*', views.django),
-    re_path(r'^display/*', views.display),
-    re_path(r'^templates/*', views.templates)
-]
