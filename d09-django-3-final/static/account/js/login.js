07ccaa66e1b676f80941229bb06a2ad54ea73593
davidmonteiro03 <damonteiro12446@gmail.com>
2025-05-19 17:15:28 +0000

    up

--- d09-django-3-final/static/account/js/login.js
+++ d09-django-3-final/static/account/js/login.js
@@ -1,24 +1,44 @@
-async function login(event) {
-	event.preventDefault();
-	const formData = new FormData(event.target);
-	const csrfToken = formData.get('csrfmiddlewaretoken');
-	formData.delete('csrfmiddlewaretoken');
-	$.ajax({
-		url: '/account/login',
-		method: 'POST',
-		headers: { 'X-CSRFToken': csrfToken },
-		data: formData,
-		processData: false,
-		contentType: false,
-	}).done(function (data) {
-		$(document.body).html(data.html);
-	}).fail(function (jqXhr) {
-		const responseJson = JSON.parse(jqXhr.responseText);
-		if (event.target.previousElementSibling === null) {
-			const newElement = document.createElement('span');
-			newElement.classList.add('text-danger', 'mb-2');
-			event.target.parentNode.insertBefore(newElement, event.target);
-		}
-		event.target.previousElementSibling.innerText = responseJson.error;
-	});
+function login(event) {
+	if (event === undefined || event === null || event === '') { return 1; }
+	try {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		const csrfToken = formData.get('csrfmiddlewaretoken');
+		formData.delete('csrfmiddlewaretoken');
+		const xhr = new XMLHttpRequest();
+		xhr.open('POST', '/account/login', true);
+		xhr.setRequestHeader('X-CSRFToken', csrfToken);
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					try {
+						const data = JSON.parse(xhr.responseText);
+						document.body.innerHTML = data.html;
+					}
+					catch (e) { console.error(`Error: ${error}`); }
+				} else {
+					try {
+						const responseJson = JSON.parse(xhr.responseText);
+						showError(event, responseJson.error);
+					}
+					catch (e) { console.error(`Error: ${error}`); }
+				}
+			}
+		};
+		xhr.send(formData);
+		return 0;
+	}
+	catch (e) {
+		console.error(`Error: ${error}`);
+		return 1;
+	}
+}
+
+function showError(event, errorMsg) {
+	if (event.target.previousElementSibling === null) {
+		const newElement = document.createElement('span');
+		newElement.classList.add('text-danger', 'mb-2');
+		event.target.parentNode.insertBefore(newElement, event.target);
+	}
+	event.target.previousElementSibling.innerText = errorMsg;
 }
--- d09-django-3-final/static/account/js/logout.js
+++ d09-django-3-final/static/account/js/logout.js
@@ -1,13 +1,39 @@
-async function logout(event) {
-	event.preventDefault();
-	const formData = new FormData(event.target);
-	const csrfToken = formData.get('csrfmiddlewaretoken');
-	formData.delete('csrfmiddlewaretoken');
-	$.ajax({
-		url: '/account/logout',
-		method: 'POST',
-		headers: { 'X-CSRFToken': csrfToken },
-	}).done(function (data) {
-		$(document.body).html(data.html);
-	});
+function logout(event) {
+	if (event === undefined || event === null || event === '') { return 1; }
+	try {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		const csrfToken = formData.get('csrfmiddlewaretoken');
+		formData.delete('csrfmiddlewaretoken');
+		const xhr = new XMLHttpRequest();
+		xhr.open('POST', '/account/logout', true);
+		xhr.setRequestHeader('X-CSRFToken', csrfToken);
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					try {
+						const data = JSON.parse(xhr.responseText);
+						document.body.innerHTML = data.html;
+					}
+					catch (e) { console.error(`Error: ${error}`); }
+				}
+			}
+		};
+		xhr.send(formData);
+		return 0;
+	}
+	catch (e) {
+		console.error(`Error: ${error}`);
+		return 1;
+	}
 }
+
+function showError(event, errorMsg) {
+	if (event.target.previousElementSibling === null) {
+		const newElement = document.createElement('span');
+		newElement.classList.add('text-danger', 'mb-2');
+		event.target.parentNode.insertBefore(newElement, event.target);
+	}
+	event.target.previousElementSibling.innerText = errorMsg;
+}
+
